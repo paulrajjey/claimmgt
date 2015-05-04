185d01c0f0ec69ed4f8ea6424e2041e670158e93
jey <>
2015-05-04 15:21:29 -0500

    {/testDoc/src/main/java/redhat/poc/testdoc/ClaimDocs.java}

--- testDoc/src/main/java/redhat/poc/testdoc/ClaimDocs.java
+++ testDoc/src/main/java/redhat/poc/testdoc/ClaimDocs.java
@@ -3,21 +3,34 @@ package redhat.poc.testdoc;
 import java.io.Serializable;
 import java.util.List;
 import org.jbpm.document.Document;
-public class ClaimDocs implements Serializable {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private List<Document> documents;
 
-	public List<Document> getDocuments() {
-		return documents;
-	}
+@org.kie.api.remote.Remotable
+public class ClaimDocs implements Serializable
+{
 
-	public void setDocuments(List<Document> documents) {
-		this.documents = documents;
-	}
-	
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 1L;
+   private List<Document> documents;
+
+   public List<Document> getDocuments()
+   {
+      return documents;
+   }
+
+   public void setDocuments(List<Document> documents)
+   {
+      this.documents = documents;
+   }
+
+   public ClaimDocs()
+   {
+   }
+
+   public ClaimDocs(java.util.List<org.jbpm.document.Document> documents)
+   {
+      this.documents = documents;
+   }
 
 }
